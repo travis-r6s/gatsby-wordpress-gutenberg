30bb2a8e70624a922a59597a5e670f623a0cbaeb
Travis Reynolds <travis@cirruscreative.co.uk>
2019-10-20 14:48:41 +0100

    update to add core blocks file

---
+++ src/core-blocks.js
@@ -0,0 +1,29 @@
+import { replaceImage } from './helpers'
+
+export const Paragraph = ({ innerHtml }) => ({ type: 'Paragraph', content: innerHtml })
+
+export const Image = async ({ json }, { wp, https }) => {
+  try {
+    const image = await replaceImage(json, { wp, https })
+    return { type: 'Image', image }
+  } catch (error) {
+    throw new Error(error.message)
+  }
+}
+
+export const Gallery = async ({ json }, { wp, https }) => {
+  const { children: imagesElements } = json.find(el => el.tagName === 'ul')
+
+  const images = []
+  // Now we can loop through that list, replacing images as usual, and pushing to an array
+  for await (const imageEl of imagesElements) {
+    try {
+      const image = await replaceImage(imageEl.children, { wp, https })
+      images.push(image)
+    } catch (error) {
+      throw new Error(error.message)
+    }
+  }
+
+  return { type: 'Gallery', images }
+}
--- src/parser.js
+++ src/parser.js
@@ -1,53 +1,44 @@
 import { parse } from 'himalaya'
 
-import { replaceImage } from './helpers'
+import CoreBlocks from './core-blocks'
 
 // Parse a block to turn it into consistent data object
-const parseBlock = async (blockContent, { node, wp, https }) => {
+const parseBlock = async (blockContent, options) => {
   const { blockName, innerHTML } = blockContent
-
+  const { node } = options
   // Check we have HTML, and parse to JSON
   if (!innerHTML) return
-  const json = parse(innerHTML)
+  blockContent.json = parse(innerHTML)
 
-  // Switch through blocks, returning each respective type
+  // A few quick checks, for blacklisted blocks etc
   if (!blockName) return { type: 'Break', content: '<br/>' }
-  if (blockName === 'core/paragraph') return { type: 'Paragraph', content: innerHTML }
-  if (blockName === 'core/image') {
-    try {
-      const image = await replaceImage(json, { wp, https })
-      return { type: 'Image', image }
-    } catch (error) {
-      throw new Error(error.message)
-    }
-  }
-  if (blockName === 'core/gallery') {
-    // We have many images - find the ul element, which contains a list of all the images
-    const { children: imagesElements } = json.find(el => el.tagName === 'ul')
-
-    const images = []
-    // Now we can loop through that list, replacing images as usual, and pushing to an array
-    for await (const imageEl of imagesElements) {
-      try {
-        const image = await replaceImage(imageEl.children, { wp, https })
-        images.push(image)
-      } catch (error) {
-        throw new Error(error.message)
-      }
-    }
-
-    return { type: 'Gallery', images }
-  }
-
   if (blockName === 'core/shortcode') {
     console.error(`Page '${node.title}' contains a shortcode block, which will be ignored.`)
     return null
   }
 
-  // Nothing? Whoops.
-  return {
-    type: '404',
-    content: innerHTML
+  /*
+  * Block Names have slugs - 'core/paragraph'
+  * We want to split this slug into two parts (category, and block), and then find the corresponding keys in an object.
+  * So we have something like { Core: { Paragraph ()..., Gallery ()... } }
+ */
+  const blockCategories = { Core: CoreBlocks }
+  const [category, block] = blockName.split('/').map(v => {
+    const [first, ...rest] = v.split('')
+    return `${first.toUpperCase()}${rest.join('')}`
+  })
+
+  try {
+    const parsedBlock = await blockCategories[ category ][ block ](blockContent, options)
+    if (parsedBlock) return parseBlock
+    else {
+      return {
+        type: '404',
+        content: innerHTML
+      }
+    }
+  } catch (error) {
+    throw new Error(error.message)
   }
 }
 
